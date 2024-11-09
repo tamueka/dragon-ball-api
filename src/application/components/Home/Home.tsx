62927acd5cab26dc5a4af3a51854be452af645dd
Samuel Ruiz de la Rosa <tamueka@hotmail.com>
2024-11-09 19:05:57 +0100

    data api + control de errores + tipado + mostrar por pantalla

--- src/application/components/Home/Home.tsx
+++ src/application/components/Home/Home.tsx
@@ -1,13 +1,57 @@
+import { useEffect, useState } from "react";
 import "./Home.css";
 
+type Character = {
+  id: number;
+  name: string;
+  ki: string;
+  maxKi: string;
+  race: string;
+  gender: string;
+  description: string;
+  image: string;
+  affiliation: string;
+  deletedAt: Date | null;
+};
+
 export const Home: React.FC = () => {
+  const urlBase = "https://dragonball-api.com/api/characters";
+  const [data, setData] = useState<Character[]>([]);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(urlBase);
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const jsonData = await response.json();
+        setData(jsonData.items);
+        console.log(jsonData.items);
+      } catch (error) {
+        console.error("Failed to fetch data:", error);
+        setError("Error fetching data. Please try again later.");
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div>
-      {" "}
-      <h1>HOME</h1>
-      <h1>HOME</h1>
-      <h1>HOME</h1>
-      <h1>HOME</h1>
+    <div className="App">
+      <h1>Personajes Dragon Ball</h1>
+      {error ? (
+        <p className="error-message">{error}</p>
+      ) : (
+        <ul className="character-list">
+          {data.slice(0, 4).map((character, index) => (
+            <li key={index}>{character.name}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
