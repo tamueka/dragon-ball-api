53fc81345c73405b14e2c6c82d4095eb31ea2491
Samuel Ruiz de la Rosa <tamueka@hotmail.com>
2024-11-10 09:48:37 +0100

    correcciones css

--- src/application/components/Footer/Footer.tsx
+++ src/application/components/Footer/Footer.tsx
@@ -3,12 +3,6 @@ import "./Footer.css";
 export const Footer: React.FC = () => {
   return (
     <footer className="footer">
-      <div className="waves">
-        <div className="wave" id="wave1"></div>
-        <div className="wave" id="wave2"></div>
-        <div className="wave" id="wave3"></div>
-        <div className="wave" id="wave4"></div>
-      </div>
       <p>&copy;2024 Samuel Ruiz | Dragon Ball API</p>
     </footer>
   );
