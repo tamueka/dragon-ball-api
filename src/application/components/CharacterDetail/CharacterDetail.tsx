8375064d543ebf88296f865a0fb8f7dabd96f29e
Samuel Ruiz de la Rosa <samuel.ruiz@babelgroup.com>
2024-11-10 22:58:59 +0100

    cambios detail + footer

--- src/application/components/CharacterDetail/CharacterDetail.tsx
+++ src/application/components/CharacterDetail/CharacterDetail.tsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { useDragonBallContext } from "../../context/DragonBallProvider";
 
 const CharacterDetail: React.FC = () => {
-  return <h2>Personajes Detalle</h2>;
+  const { characters } = useDragonBallContext();
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const character = characters.find(
+    (character) => character.id === parseInt(id)
+  );
+
+  if (!character) {
+    return <p>Personaje no encontrado</p>; 
+  }
+
+  return (
+    <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+      <h2>{character.name}</h2>
+      <img src={character.image} alt={character.name} style={{height: "80%", width: "80%"}}/>
+      <p>{character.description}</p>
+      <p>{character.ki}</p>
+      <p>{character.maxKi}</p>
+
+      <button onClick={() => navigate('/')}>Volver</button>
+
+    </div>
+  );
 };
 
 export default CharacterDetail;
