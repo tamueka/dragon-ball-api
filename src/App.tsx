f7e53954fbfe76bfec4337954c979ad0751aeeef
Samuel Ruiz de la Rosa <tamueka@hotmail.com>
2024-11-10 22:03:56 +0100

    correguimos error router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
-import { Route } from "react-router-dom";
-import Character from "./application/components/Character/Character";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import CharacterList from "./application/components/CharacterList/Characterlist";
 import { Footer } from "./application/components/Footer/Footer";
 import { Header } from "./application/components/Header/Header";
@@ -9,17 +8,19 @@ import { DragonBallProvider } from "./application/context/DragonBallProvider";
 export const App: React.FC = () => {
   return (
     <DragonBallProvider>
-      <Header />
-      <div>
-        <h1 style={{ textAlign: "center" }}>Personajes Dragon Ball</h1>
-        {/* <Search /> */}
-        {/* Ruta para la lista de personajes */}
-        <Route path="/" element={<CharacterList />} />
-        {/* Ruta para los detalles de un personaje específico */}
-        <Route path="/character/:id" element={<Character />} />
-        {/*         <Favorites /> */}
-      </div>
-      <Footer />
+      <Router>
+        <Header />
+        <div>
+          <h1 style={{ textAlign: "center" }}>Personajes de Dragon Ball</h1>
+          <Routes>
+            {/* Ruta principal para la lista de personajes */}
+            <Route path="/" element={<CharacterList />} />
+            {/* Ruta dinámica para los detalles de un personaje específico */}
+            {/*  <Route path="/character/:id" element={<CharacterDetail />} /> */}
+          </Routes>
+        </div>
+        <Footer />
+      </Router>
     </DragonBallProvider>
   );
 };
